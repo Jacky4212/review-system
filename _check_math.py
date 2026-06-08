25a5e5437c869b9e9cb4661c5bee48202a1f7138
Jacky4212 <jackyjacky4212@qq.com>
2026-06-08 20:56:47 +0800

    cleanup: 移除误提交的PPT提取图片

--- _check_math.py
+++
@@ -1,8 +0,0 @@
-with open(r'D:\code\cherry studio\复习\index.html', 'r', encoding='utf-8') as f:
-    content = f.read()
-if 'formulasHTML' in content:
-    idx = content.find('formulasHTML')
-    # Print the next 300 chars
-    print(content[idx:idx+400])
-else:
-    print('formulasHTML not found')
